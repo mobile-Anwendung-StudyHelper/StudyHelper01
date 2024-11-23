844c54f20268dc6a5806d73d3ed56dc24763a51b
the-one-and-only-niels <Niels.Fricke@t-online.de>
2024-11-23 14:06:41 +0100

    modulplan adapter schöner 2.0

--- CourseCrafter_Selfmade/app/src/main/java/com/nfricke/coursecrafter_selfmade/Adapter/ModulplanAdapter.java
+++ CourseCrafter_Selfmade/app/src/main/java/com/nfricke/coursecrafter_selfmade/Adapter/ModulplanAdapter.java
@@ -1,7 +1,5 @@
 package com.nfricke.coursecrafter_selfmade.Adapter;
 
-import static java.security.AccessController.getContext;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
